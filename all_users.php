5b287c89e0e6999fff8cfbeaf1693101cf971f45
DEMBA-ELIMANE KANE <demba-elimane.kane@iut-rodez.fr>
2019-11-06 12:09:20 +0100

    fix #2 Récupération et affichage des utilisateurs avec énoncé non prépar

---
+++ all_users.php
@@ -0,0 +1,45 @@
+<!DOCTYPE HTML>
+<body>
+<head>
+	<title>All User</title>
+	<meta charset="utf-8">
+<head>
+<body>
+	<h1><b>All user</b></h1>
+		
+		<table border="1">
+			<tr>
+				<b>
+				<td>id</td>
+				<td>UserName</td>
+				<td>Email</td>
+				<td>Status</td>
+				</b>
+			</tr>
+			<?php 
+				$host='localhost';
+				$db='my_activities';
+				$user='root';
+				$pass='root';
+				$charset='utf8mb4';
+				$dsn="mysql:host=$host;dbname=$db;charset=$charset";
+				$options=[
+					PDO::ATTR_ERRMODE				=>PDO::ERRMODE_EXCEPTION,
+					PDO::ATTR_DEFAULT_FETCH_MODE	=>PDO::FETCH_ASSOC,
+					PDO::ATTR_EMULATE_PREPARES		=>false,];
+				try{
+					$pdo=new PDO($dsn,$user,$pass,$options);
+				}catch(PDOException$e){
+					throw new PDOException($e->getMessage(),(int)$e->getCode());
+				}
+				$stmt = $pdo->query('SELECT * FROM users ORDER BY username');
+				while($row = $stmt->fetch()){
+					echo "<tr>";
+					echo "<td>".$row['id']."</td>";
+					echo "<td>".$row['username']."</td>";
+					echo "<td>".$row['email']."</td>";
+					echo "<td>".$row['status_id']."</td>";
+					echo "</tr>";
+				}
+			?>
+		</table>
